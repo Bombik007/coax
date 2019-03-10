0aea851f7b5f67faa56d19f61dae3434675d702e
Bombik007 <bodiababiak@gmail.com>
2019-03-11 00:29:43 +0200

    Add localStorage.setItem after reordering list

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,7 +4,7 @@ import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 import { Button, Thumbnail } from '../components'
 
 const reorderList = (list, startIndex, endIndex) => {
-    const result = Array.from(list)
+    const result = [...list]
     const [removed] = result.splice(startIndex, 1)
     result.splice(endIndex, 0, removed)
     return result
@@ -14,13 +14,11 @@ const Dashboard = ({userList, dispatch, history}) => {
     let [list, setList] = useState(userList)
     
     useEffect(() => {
-        if (!userList.length) {
-            const rawData = localStorage.getItem('ccx')
-            if (rawData !== null) {
-                const data = JSON.parse(rawData)
-                setList(data)
-                dispatch({type: 'SET_LIST', userList: data})
-            }
+        const rawData = localStorage.getItem('ccx')
+        if (rawData !== null) {
+            const data = JSON.parse(rawData)
+            setList(data)
+            dispatch({type: 'SET_LIST', userList: data})
         }
     }, [])
   
@@ -36,60 +34,61 @@ const Dashboard = ({userList, dispatch, history}) => {
         history.push('/user')
     }
 
-    const onDragEnd = (result) => {
-        if (!result.destination) return
+    const onDragEnd = ({destination, source}) => {
+        if (!destination) return
 
         const items = reorderList(
             list,
-            result.source.index,
-            result.destination.index
+            source.index,
+            destination.index
         )
         setList(items)
+        localStorage.setItem('ccx', JSON.stringify(items))
     }
 
     return (
-      <>
-      <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId="droppable" direction="horizontal">
-          {({droppableProps, innerRef, placeholder}) => (
-            <div
-              className='testList'
-              ref={innerRef}
-              {...droppableProps}
-            >
-              {
-                list.map((el, index) => (
-                <Draggable 
-                  key={el.id} 
-                  draggableId={el.id} 
-                  index={index}
-                >
-                  {({draggableProps, dragHandleProps, innerRef}) => (
-                    <div
-                      className='testItem'
-                      ref={innerRef}
-                      {...draggableProps}
-                      {...dragHandleProps}
-                    >
-                        <Thumbnail
-                            key={el.id}
-                            {...el} 
-                            onClick={selectUser}
-                        />
-                    </div>
-                  )}
-                </Draggable>
-              ))
-              }
-              {placeholder}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
-      <Button text='Add Contact' onClick={goToForm}/>
-      </>
+        <>
+            <DragDropContext onDragEnd={onDragEnd}>
+                <Droppable droppableId="droppable" direction="horizontal">
+                    {({droppableProps, innerRef, placeholder}) => (
+                        <div
+                            className='testList'
+                            ref={innerRef}
+                            {...droppableProps}
+                        >
+                        {
+                            list.map((el, index) => (
+                            <Draggable 
+                                key={el.id} 
+                                draggableId={el.id} 
+                                index={index}
+                            >
+                                {({draggableProps, dragHandleProps, innerRef}) => (
+                                    <div
+                                        className='testItem'
+                                        ref={innerRef}
+                                        {...draggableProps}
+                                        {...dragHandleProps}
+                                    >
+                                        <Thumbnail
+                                            key={el.id}
+                                            {...el} 
+                                            onClick={selectUser}
+                                        />
+                                    </div>
+                                )}
+                            </Draggable>
+                            ))
+                        }
+                        {placeholder}
+                        </div>
+                    )}
+                </Droppable>
+            </DragDropContext>
+            <Button text='Add Contact' onClick={goToForm}/>
+        </>
     )
-  }
+}
 
 const mapStateToProps = store => ({userList: store.userList})
 
